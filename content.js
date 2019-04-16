da35bc3dc91590adbe405bb37b0aa5e2265c2001
Julia Zamaitat <j.zamaitat@gmail.com>
2019-04-16 21:55:35 +0200

    Filtering for a special Regex on a webpage works, shows all the div where the word is contained. Need to address the innermost divs only.

--- content.js
+++ content.js
@@ -1,10 +1,20 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse){
         if(request.message === "clicked_browser_action"){
-            var firstLink = document.getElementsByTagName('a')[0].href;
-            console.log(firstLink);
+           // var firstLink = document.getElementsByTagName('a')[0].href;
+            //console.log(firstLink);
+            const re = new RegExp('Game of Thrones')
+            
+            var divs = document.getElementsByTagName("div");
+            for(var i = 0; i < divs.length; i++){
+                if(divs[i].innerHTML.match(re)){
+                    console.log(divs[i]);
+                }
+            }
 
-            chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstLink});
+            
+
+            //chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstLink});
         }
     }
 );
