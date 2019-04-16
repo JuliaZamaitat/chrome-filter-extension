6554fcc9aa5c9d0341de0fa1da86c7eefaed740f
Julia Zamaitat <j.zamaitat@gmail.com>
2019-04-16 20:54:19 +0200

    Extension listens for click on icon and then opens the first link found on the webpage.

--- content.js
+++ content.js
@@ -1,3 +1,10 @@
-//var firstLink = document.getElementsByTagName('a')[0].href;
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+        if(request.message === "clicked_browser_action"){
+            var firstLink = document.getElementsByTagName('a')[0].href;
+            console.log(firstLink);
 
-//console.log(firstLink);
+            chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstLink});
+        }
+    }
+);
